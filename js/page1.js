e5866af430a17b5771a31e9f32ed7fc217ac5eee
1F10260005 <s1f102600057@iniad.org>
2026-06-26 21:02:37 +0900

    スライダーとボタン式表示システムを実装

---
+++ js/page1.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('.slider').slick({
+      arrows: true, // 前・次のボタンを表示する
+      dots: true, // ドットナビゲーションを表示する
+      appendDots: $('.dots-2'), // ドットナビゲーションの生成位置を変更
+      speed: 1000, // スライドさせるスピード（ミリ秒）
+      slidesToShow: 1, // 表示させるスライド数
+      centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
+      variableWidth: true, // スライド幅の自動計算を無効化
+    });
+});
+
+
+const con = document.getElementsByClassName("page");
+
+function show(a) {
+  for (let i = 0; i < con.length; i++) {
+    if (i === a) {
+      con[i].style.display = "block";
+    } else {
+      con[i].style.display = "none";
+    }
+  }
+}
